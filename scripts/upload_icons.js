5d9eb0c44bb900d9db5cef44e14171c05ece3f0e
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-11-18 22:44:45 +0100

    split upload workflows

--- scripts/upload_icons.js
+++ scripts/upload_icons.js
@@ -4,24 +4,28 @@ const path = require('path')
 const { Storage } = require('@google-cloud/storage')
 
 // Configuration
-const BUCKET_NAME = 'technology_detections'
+const BUCKET_NAME = 'httparchive' // CDN bucket, available as https://cdn.httparchive.org/
+const BUCKET_PATH = 'static/icons/' // Path in the bucket where the icons should be uploaded
 const ICONS_DIR = path.resolve(__dirname, '../src/images/icons/converted') // Local directory where your PNG icons are stored
 
-const storage = new Storage({
-  keyFilename: '/tmp/gcp_key.json',
-})
+const storage = new Storage()
 
 async function syncIcons() {
+  console.log('Syncing icons...')
   const bucket = storage.bucket(BUCKET_NAME)
 
   // Get list of files in the bucket
-  const [filesInBucket] = await bucket.getFiles()
+  const [filesInBucket] = await bucket.getFiles({
+    prefix: BUCKET_PATH,
+  })
   const bucketFilesMap = new Map(
     filesInBucket.map((file) => [
       file.name,
       new Date(file.metadata.updated).getTime(),
     ])
   )
+  console.log(`Found ${bucketFilesMap.size} files in the bucket.`)
+
 
   // Read all files from the local icons directory
   const localFiles = fs
@@ -38,21 +42,17 @@ async function syncIcons() {
       !fileInBucketUpdatedTime ||
       fileMetadata.mtime.getTime() > fileInBucketUpdatedTime
     ) {
-      try {
-        await bucket.upload(filePath, {
-          destination: 'icons/' + file,
-          metadata: {
-            contentType: 'image/png',
-          },
-        })
-        console.log(`Uploaded: ${file}`)
-      } catch (err) {
-        console.error(`Error uploading file ${file}:`, err)
-      }
+      await bucket.upload(filePath, {
+        destination: BUCKET_PATH + file,
+        metadata: {
+          contentType: 'image/png',
+        },
+      })
+      console.log(`Uploaded: ${file}`)
     } else {
       console.log(`File already exists and is up to date: ${file}`)
     }
   }
 }
 
-syncIcons().catch(console.error)
+syncIcons()
